4e10c532b5ad0500270fd2000edd7df79eb3783e
Mateo Castillo <mate.dev9618@gmail.com>
2023-03-21 15:34:58 -0500

    Update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store";
 import { LoginRegister, RegisterPage, FeedbackPage, BoardPage } from "./pages";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route index element={<LoginRegister />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/feedback/all" element={<FeedbackPage />} />
-          <Route path="/feedback/:title" element={<BoardPage />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route index element={<LoginRegister />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/feedback/all" element={<FeedbackPage />} />
+        <Route path="/feedback/:title" element={<BoardPage />} />
+      </Routes>
+    </Router>
   );
 }
 
